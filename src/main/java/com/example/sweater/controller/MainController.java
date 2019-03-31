c401d4acf2131d36409644c6ba01a57d5fc702be
VitaliyKavun <kavunv@mydigicode.com>
2019-04-01 00:07:47 +0300

    initial commit

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,67 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("main")
+    public String main(Map<String, Object> model){
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("main")
+    public String addMessage(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model){
+        Message message = new Message(text, tag, user);
+
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+       // model.put("author", message.getAuthor());
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String findMessage(@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messageList;
+        if(filter != null && !filter.isEmpty()){
+            messageList = messageRepository.findByTag(filter);
+        }else{
+            messageList = messageRepository.findAll();
+        }
+
+        model.put("messages", messageList);
+
+
+        return "main";
+    }
+
+}
